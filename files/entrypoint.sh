967379ab400389aae0d5312331c4bbe0438df6b0
nightelfwalk <141609007+nightelfwalk@users.noreply.github.com>
2023-08-31 09:34:05 +0800

    Update entrypoint.sh

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -2,7 +2,7 @@
 
 # 设置各变量
 WSPATH=${WSPATH:-'argo'}
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
+UUID=${UUID:-'862619e6-d39e-4bce-9093-67d5163af1be'}
 WEB_USERNAME=${WEB_USERNAME:-'admin'}
 WEB_PASSWORD=${WEB_PASSWORD:-'password'}
 
